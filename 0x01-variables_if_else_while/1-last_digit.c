a814e4710239ef885e86cb2a6ddd62e88bcfac89
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-09 22:46:39 +0100

    the last scripts

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,14 +17,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 Last_digit = n % 10;
 if (Last_digit > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n, Last_digit);
+printf("Last digit of %d is %d and is greater than 5" n, Last_digit);
 else if (Last_digit == 0)
 {
-printf("Last digit of %d is 0 and is 0\n", n);
+printf("Last digit of %d is 0 and is 0" n);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, Last_digit);
+printf("Last digit of %d is %d and is less than 6 and not 0" n, Last_digit);
 }
 return (0);
 }
