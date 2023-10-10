15dbfcaff38d8bb07dcbed3c1fb7e56d15447e9f
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-10 21:10:50 +0100

    the last scripts

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+/**
+ *main - calls other functions
+ *This code prints all English alphabets in lower case
+ *and in reverse
+ *Author- Qamar
+ *Return: 0 for success
+ */
+int main(void)
+{
+char letter = 'z';
+while (letter >= a)
+{
+putchar(letter);
+letter--;
+}
+putchar('\n');
+}
