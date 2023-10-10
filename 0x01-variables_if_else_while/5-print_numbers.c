63858e910d5efa457c1ed6214c711a20a7ca8349
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-10 05:25:22 +0100

    the last scripts

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - calls other functions
+*This code prints all base 10 digits from
+*0 to 10
+*Author- Qamar
+*Return: 0 always success
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 10)
+{
+printf("%d\n", digit;)
+digit++;
+}
+return (0);
+}
