1b358a47318f35b19de29fcf4670123659a1f4c7
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-10 03:29:48 +0100

    the last scripts

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - calls other functions
+ *This code prints all English alphabets in lower case
+ *Author- Qamar
+ *Return: 0 always success
+ */
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
