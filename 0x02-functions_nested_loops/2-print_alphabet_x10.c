aad8b345cc88c78a5b0970b367109f471c057abe
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-12 00:32:26 +0100

    the last scripts of and programs of functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+include "main.h"
+
+/**
+ *print_alphabet_x10 - prints the  alphabets in lower case
+ *10 times followed by a new line
+ *Author-Qamar
+ *Return: void
+ */
+void print_alphabet_x10(void)
+{
+int i, j;
+j = 0;
+while (j < 10)
+{
+for (i = 97; i <= 122; i++)
+_putchar(i);
+_putchar('\n');
+j++;
+}
+}
