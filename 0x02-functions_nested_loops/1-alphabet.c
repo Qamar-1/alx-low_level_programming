be089806b5f47aaada11efb2f40a44f36902081b
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-11 21:13:54 +0100

    the last scripts of and programs of functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "<main.h>"
+/**
+*main - Entry point | calls other functions
+*This code prints all English alphabets in lower case!
+*Author- Qamar
+*Return: 0  success
+*/
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+}
