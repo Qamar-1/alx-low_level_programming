01f9aae2f6924a683765ddeb98ef3fb7b3595110
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-12 14:13:27 -0700

    the last scripts

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,9 @@
 
 /**
 *main - tests function that prints if integer is positive or negative
-*Return: 0
+*positive_or_negative - test fuction
+*Author- Qamar
+*Return: 0 success
 */
 int main(void)
 {
