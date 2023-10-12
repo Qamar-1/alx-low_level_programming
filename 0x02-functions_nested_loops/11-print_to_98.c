094bb0aa05f1845ae8ee946a0ab0102a9a046681
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-12 03:17:12 +0100

    the last scripts of and programs of functions and nested loops betty 10-add.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_to_98 - print any number to 98
+*@n: number to print
+*Author-EngineerPhoenix
+*Return: void
+*/
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+while (n <= 98)
+{
+while (n <= 98)
+{printf("%d", n);
+if (n < 98)
+{
+printf(", ");
+if (n > 98)
+{
+printf(", ");
+} n++
+}
+}
+else
+{
+while (n >= 98)
+}
+printf("%d", n);
+if (n > 98)
+{
+printf(", ");
+}
+n--;
+}
+printf("\n");
+}
