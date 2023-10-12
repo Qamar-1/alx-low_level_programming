c11fb936e405533d1ad8f024b49bade2c6410221
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-12 19:43:29 +0100

    the last scripts of and programs of functions and nested loops

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - call other functions
+ *This code checks the last number and print whether
+ * it is zero, positive or negative
+ *Author-EngineerPhoenix
+ *Return: 0 always success
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
