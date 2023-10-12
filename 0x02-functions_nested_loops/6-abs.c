0e2cca4b4062c9376168a5bc6808a83cec9ed631
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-12 01:14:20 +0100

    the last scripts of and programs of functions and nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*_abs-return the absolute value of a number
+*@n: number to be checked
+*Author-Qamar
+*Return: positive value of n
+*/
+int _abs(int n)
+{
+return ((n < 0) ? -1 * n : n);
+}
