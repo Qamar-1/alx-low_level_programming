3d878ade36d16e515bb3a5c5a3a519cbedf63bac
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-13 22:31:31 +0100

    the last scripts have been tested and updated

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul: multiplies 2 numbers
+ * @a: first number
+ * @b: second number
+ * Author- Qamar
+ *Return: 0  success
+ */
+int mul(int a, int b)
+{
+int result = a * b;
+return result;
+}
