f1cfe34866b63370c6e05892ca7dbef98a0a24e1
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-13 18:15:39 -0700

    project round off scripts

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
-
 /**
- * print_line - prints a straight line
- * @n : number of reference
- * Author- Qamar
- * Return: 0  success
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
-{
-int i;
-for (i = 0; i <= n; i++)
 {
 if (n <= 0)
 {
 _putchar('\n');
+} else
+{
+int i;
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
 }
-else
-_putchar('-')
+_putchar('\n');
 }
 }
