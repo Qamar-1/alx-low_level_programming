65d78bb1ec8ccde556a6f95af70eb8b6f39b00cc
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-14 02:04:01 +0100

    the last scripts have been tested and updated

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * print_line - prints a straight line
+ * @n : number of reference
+ * Author- Qamar
+ * Return: 0  success
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+_putchar('-')
+}
+}
