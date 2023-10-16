e67dd319c264d57cdfbb02a96f0cbc5175d225a5
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-16 22:11:23 +0100

    Written the programs for the 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ *swap_int - swaps 2 numbers
+ *@a :first number
+ *@b :second number
+ *Author- Qamar
+ *Return: 0  success
+ */
+void swap_int(int *a, int *b)
+{
+int A = *a;
+*b = *a;
+*a = A;
+}
