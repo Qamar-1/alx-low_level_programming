22c11579494d1c265e0b7211e20d55fce9787645
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-16 23:49:48 +0100

    Written the programs for the 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+/**
+ *_strlen -This is   to check the last string character
+ *@s :the string to be
+ *Author- Qamar
+ *Return: 0  success
+ */
+
+
+
+int _strlen(char *s)
+{
+int last = 0;
+for (int i = 0; s[i] != '\0'; i++)
+{
+last = s[i];
+}
+return (last);
+}
