5de0955f31b7774c6b282793dd7949c57ea6d3f1
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-17 07:30:31 +0100

    Written the programs for the 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+/**
+ *void _puts - prints a string
+ *This code prints all the characters of a string
+ *@s: The char to be printed 
+ *Author- Qamar
+ */
+
+void _puts(char *str){
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+putchar('\n');
+}
