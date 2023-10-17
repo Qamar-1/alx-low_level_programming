ecf3905e9f5b5861be055776c3291ef7b38c2366
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-17 21:30:34 +0100

    Written the programs for the 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+/**
+ *print_rev - the fuction created
+ *@s - the string to be printed
+ *This function prints all the string
+ *characters and in reverse 
+ *Author- Qamar
+ *Return: 0  success
+ */
+void print_rev(char *s) {
+
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+i--;
+for (; i >= 0; i--){
+if (i == 0){
+_putchar('\n');
+}
+else 
+_putchar(s[i]);
+}
+}
