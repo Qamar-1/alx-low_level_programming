b81490ece3a74c6591584e1079b52a96dd3390fa
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-18 01:41:51 +0100

    Written the programs for the 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print an array of integers
+ * @a: array to print
+ * @n: number of elements
+ * Author: Qamar
+ * Return: void
+ */
+
+
+void print_array(int *a, int n)
+{
+int i;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+i < n - 1 ? printf("%d, ", *(a + i)) : printf("%d\n", *(a + i));
+}
+}
