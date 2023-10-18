49f36ac3000d414adefcbcf19c67739c1f02a9d8
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-18 02:09:51 +0100

    Written the programs for the 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/** 
+ * _strlen - get the length of a string
+ * @s: string to get length
+ * Author: Qamar
+ * Return: length
+ */
+int _strlen(char *s)
+{
+char *cpy;
+int length;
+
+for (cpy = s, length = 0; *cpy != '\0'; cpy++)
+length++;
+return (length);
+}
+
+/**
+ * puts_half -  print the other half of a string
+ * @str: string to print
+ * Author: Qamar
+ *Return: void
+ */
+void puts_half(char *str)
+{
+int n;
+n = _strlen(str) % 2 == 0 ? _strlen(str) / 2 : (_strlen(str) - 1) / 2;
+for (; n < _strlen(str); n++)
+_putchar(*(str + n));
+_putchar('\n');
+}
