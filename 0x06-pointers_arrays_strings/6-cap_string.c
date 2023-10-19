bb11f4e5513543437794f30ef8222e134c70dcdf
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-19 22:22:37 +0100

    Written the programs for the 0x05-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,22 +5,14 @@
  * Return: the pointer to dest.
  */
 
-char *cap_string(char *s)
-{
-int count = 0, i;
-int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-if (*(s + count) >= 97 && *(s + count) <= 122)
-*(s + count) = *(s + count) - 32;
-count++;
-while (*(s + count) != '\0')
-{
-for (i = 0; i < 13; i++)
-{
-if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-*(s + (count + 1)) = *(s + (count + 1)) - 32;
-break;
+char *cap_string(char *s) {
+int i = 0;
+while (s[i] != '\0') {
+if ((i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n') && (s[i] >= 'a' && s[i] <= 'z')) {
+s[i] = s[i] - ('a' - 'A');
 }
-count++;
+i++;
 }
-return (s);
+
+return s;
 }
