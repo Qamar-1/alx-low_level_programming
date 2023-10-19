9d32ffac5083799b6b34dac6dc91755bf6e043b6
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2023-10-19 22:26:40 +0100

    Written the programs for the 0x05-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,14 +5,18 @@
  * Return: the pointer to dest.
  */
 
-char *cap_string(char *s) {
+char *cap_string(char *s)
+{
 int i = 0;
-while (s[i] != '\0') {
-if ((i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n') && (s[i] >= 'a' && s[i] <= 'z')) {
+while (s[i] != '\0')
+{
+if ((i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n') &&
+(s[i] >= 'a' && s[i] <= 'z'))
+{
 s[i] = s[i] - ('a' - 'A');
 }
 i++;
 }
 
-return s;
+return (s);
 }
