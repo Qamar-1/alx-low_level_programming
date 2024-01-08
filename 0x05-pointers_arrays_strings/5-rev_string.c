9bd9fb2a187f77c2000ddf9ed3ec7ab5cd3d3152
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-08 16:47:57 +0100

    The function here prints the string in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,5 @@
 #include "main.h"
 
-/**
- *_strlen - get the length of a string
- *@s: string in question
- *Author: Qamar
- *Return: length
- */
-
-int _strlen(char *s)
-{
-int length;
-
-for (s, length = 0; *s != '\0'; s++)
-length++;
-return (length);
-}
-
 
 
 /**
