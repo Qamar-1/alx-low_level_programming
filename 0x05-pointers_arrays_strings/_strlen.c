99f24e17c773182f6414076041a397dfcd7c2da1
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-09 22:35:05 +0100

    Refactor puts2 function for correct iteration and character printing

---
+++ 0x05-pointers_arrays_strings/_strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *_strlen -This is   to check the last string character
+ *@s :the string to be
+ *Author- Qamar
+ *Return: 0  success
+ */
+int _strlen(char *s)
+{
+int length = 0;  // stores the length.
+int i;
+for (i = 0; s[i] != '\0'; i++)
+length++;
+return (length);
+}
+int main()
+{
+
+}
