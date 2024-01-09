f376eb5a5b41dc61f241de72b22b45cf2554090b
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-09 21:53:12 +0100

    Refactor puts2 function for correct iteration and character printing

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,31 +1,13 @@
-#include "main.h"
-/**
- * _strlen - get the length of a string
- * @s: string to get length
- * Author: Qamar
- * Return: length
- */
-int _strlen(char *s)
+#include <stdio.h>
+#include <string.h>
+void half(char *s)
 {
-char *cpy;
-int length;
-
-for (cpy = s, length = 0; *cpy != '\0'; cpy++)
-length++;
-return (length);
+if (strlen(s) % 2 != 0)
+{
+int n = (strlen(s) - 1) / 2;
 }
-
-/**
- * puts_half -  print the other half of a string
- * @str: string to print
- * Author: Qamar
- *Return: void
- */
-void puts_half(char *str)
-{
-int n;
-n = _strlen(str) % 2 == 0 ? _strlen(str) / 2 : (_strlen(str) - 1) / 2;
-for (; n < _strlen(str); n++)
-_putchar(*(str + n));
-_putchar('\n');
+int n = strlen(s) / 2;
+for (; s[n] != '\0'; n++)
+putchar(s[n]);
+putchar('\n');
 }
