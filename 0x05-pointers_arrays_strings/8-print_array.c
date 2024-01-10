3fb9b21334ccd13e039627346a9d6de8980925d1
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-10 14:10:44 +0100

    Function updated with new feature

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,6 +11,8 @@
 
 void print_array(int *a, int n)
 {
+if (n > 0)
+{
 int i;
 for (i = 0; i < n; i++)
 {
@@ -19,4 +21,7 @@ if (i < (n - 1))
 else
 	printf("%d\n",a[i]);
 }
+}
+else 
+printf("Array is empty");
 }
