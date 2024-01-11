45f0843236371ecd1ae241d5dc66656db2873915
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-11 22:39:10 +0100

    recusive functions updated and edge cases

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,9 @@
-
-#include <stdio.h>
-#include "main.h"
+/**
+* _puts_recursion - Entry point | calls other functions
+*This code prints outs the string
+*Author- Qamar
+*Return: 0  success
+*/
 void _puts_recursion(char *s)
 {
 if (*s != '\0')
@@ -8,6 +11,7 @@ if (*s != '\0')
 _putchar(*s);
 _puts_recursion(s+1);
 }
+else
 _putchar('\n');
 }
 
