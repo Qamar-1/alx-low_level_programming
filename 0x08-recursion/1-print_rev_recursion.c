c4cfbedb3d67c671b97bdfcf396b5c72a15fce48
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-12 09:24:34 +0100

    recursion order sorted

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * print_reverse_recursive - Prints a string in reverse order using recursion.
+ * @s: Pointer to the string to be printed in reverse.
+ * This function uses recursion to print the characters of a string in reverse order.
+ */
+#include "main.h"
+void _print_rev_recursion(char *s)
+{
+if (*s != '0/')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+
