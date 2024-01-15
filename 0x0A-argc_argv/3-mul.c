e501b8d2641f93997964a3af084144efae90f875
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-15 17:23:17 +0100

    Code efficiency improved. Output merged

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies the first 2 args
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc,  char *argv[] __attribute__((unused)) )
+{
+if (argc == 3)
+printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+else
+printf("Error\n");
+return (0);
+}
