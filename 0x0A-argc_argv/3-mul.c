bcb5991e9c1bc8c92f5f28420550c4cdf0ab61f8
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-15 17:25:56 +0100

    Code efficiency improved. Output merged

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,6 +11,9 @@ int main(int argc,  char *argv[] __attribute__((unused)) )
 if (argc == 3)
 printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
 else
+{
 printf("Error\n");
+return (1);
+}
 return (0);
 }
