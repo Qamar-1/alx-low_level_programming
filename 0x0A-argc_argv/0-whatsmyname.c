b35d2c04fb11346464e42cc6152a762d180dff2c
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-15 14:45:08 +0100

    file location updated and tested

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+/**
+ main - Entry point | calls other functions
+ *This code prints its own name
+ *Author- Qamar
+ *Return: 0  success
+ */
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+printf("The name of the program is %s\n", *(argv));
+}
