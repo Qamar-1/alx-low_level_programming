49bce3e9d70fdec0a9ff8c145d94da6a106b17c7
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-15 15:04:07 +0100

    file location updated and tested

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,8 @@
  *Return: 0  success
  */
 #include<stdio.h>
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
 {
-printf("The name of the program is %s\n", *(argv));
+printf("The name of the program is %s\n", argv[0]);
+return (0);
 }
