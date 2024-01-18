abf41bcf7158bd5167720e29cfdad3aa33bdcf86
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-18 05:18:26 -0800

    memory allocated dynamically for arrrays

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 char *create_array(unsigned int size, char c)
 {
-int i;
+unsigned int i;
 char* arr;
 if (size == 0)
 return NULL;
 arr = (char*)malloc(size + 1);
 if (arr == NULL)
 return NULL;
-unsigned  int i;
 for (i = 0; i < size; i++)
 arr[i] = c;
 return (0);
