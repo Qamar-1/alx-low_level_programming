ab09f96fa0b99e2a06f7f3cd8ef46c4a0d839e4d
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-22 19:55:37 +0100

    String manipulation completed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,37 +3,38 @@
 int **alloc_grid(int width, int height)
 {
 	    int i, j;
+	        int **grid;
 
-	        if (width <= 0 || height <= 0)
-			        return NULL;
+		    if (width <= 0 || height <= 0)
+			            return NULL;
 
-		    int **grid = (int **)malloc(height * sizeof(int *));
-		        
-		        if (grid == NULL)
-				        return NULL;
-
-			    for (i = 0; i < height; i++)
-				        {
-						        grid[i] = (int *)malloc(width * sizeof(int));
-
-							        if (grid[i] == NULL)
-									        {
-											            for (j = 0; j < i; j++)
-													                    free(grid[j]);
-
-												                free(grid);
-														            return NULL;
-															            }
-								    }
+		        grid = (int **)malloc(height * sizeof(int *));
+			    
+			    if (grid == NULL)
+				            return NULL;
 
 			        for (i = 0; i < height; i++)
 					    {
-						            for (j = 0; j < width; j++)
-								            {
-										                grid[i][j] = 0;
-												        }
-							        }
+						            grid[i] = (int *)malloc(width * sizeof(int));
 
-				    return grid;
+							            if (grid[i] == NULL)
+									            {
+											                for (j = 0; j < i; j++)
+														                free(grid[j]);
+
+													            free(grid);
+														                return NULL;
+																        }
+								        }
+
+				    for (i = 0; i < height; i++)
+					        {
+							        for (j = 0; j < width; j++)
+									        {
+											            grid[i][j] = 0;
+												            }
+								    }
+
+				        return grid;
 }
 
