5bdfcf947e0cd1bf1f61f96e99e1032684dc1f2a
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-22 15:37:56 +0100

    String manipulation completed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,11 +12,11 @@ int i;
 int s1_length = _strlen(s1);
 int s2_length = _strlen(s2);
 int s3_length = s1_length + s2_length + 1;
+char* s3 = (char*)malloc(s3_length);
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s1 = "";
-char* s3 = (char*)malloc(s3_length);
 if (s3 == NULL)
 return NULL;
 for (i = 0; i < s1_length; i++)
