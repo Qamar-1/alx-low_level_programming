c13353e0586d874ad2e2c7355f02eface9fa6665
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-22 15:34:32 +0100

    String manipulation completed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
-#include "stdio.h"
+#include <stdlib.h>
+#include <main.h>
 /**
  *str_concat - Concatenates two strings.
  *@s1: The first input string.
@@ -8,13 +9,13 @@
 char *str_concat(char *s1, char *s2)
 {
 int i;
-int s1_length = strlen(s1);
-int s2_length = strlen(s2);
+int s1_length = _strlen(s1);
+int s2_length = _strlen(s2);
 int s3_length = s1_length + s2_length + 1;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
-s1 == "";
+s1 = "";
 char* s3 = (char*)malloc(s3_length);
 if (s3 == NULL)
 return NULL;
