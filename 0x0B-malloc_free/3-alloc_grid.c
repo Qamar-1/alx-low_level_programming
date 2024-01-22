4959c7d48e53abdba8b9835af7c456c7121425c9
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-22 19:37:39 +0100

    String manipulation completed

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Allocates memory for a 2D array of integers and initializes
+ * each element to 0.
+ * @width: The number of elements in each row (width of each 1D array).
+ * @height: The number of rows (number of 1D arrays).
+ * Return: If successful, a pointer to the allocated 2D array; otherwise, NULL.
+ */
+int **alloc_grid(int width, int height)
+{
+int i,j;
+int** grid= (int**)malloc(height * sizeof(int*));
+if (width <= 0 || height <= 0)
+return NULL;
+if (grid == NULL)
+return NULL;
+for (i = 0; i < height; i++)
+grid[i] = (int*)malloc(width* sizeof(int*);
+if(grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+free(grid[j]);
+free(grid);
+return NULL;
+}
+
+for (int i = 0; i < height; i++) {
+ for (int j = 0; j < width; j++) {
+grid[i][j] = 0;
+}
+ }
+return grid;
+
+}
+
