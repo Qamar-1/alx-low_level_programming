d6702d455d621e013f57ac10ac12caf306b5b4d8
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-23 19:50:23 +0100

    All structures defined and initialized

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/*
+ * struct dog - Represents information about a dog
+ * @name: Name of the dog (string)
+ * @age: Age of the dog (float)
+ * @owner: Owner of the dog (string)
+ */
+struct dog {
+char *name;
+float age;
+char *owner;
+};
+
+#endif
