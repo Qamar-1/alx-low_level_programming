f33d2b59d08db6ded8f4251141d9c5d5119432f2
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-30 20:33:21 +0100

    variadic functions implemented

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,38 @@
+/*
+ * File: 1-print_numbers.c
+ * Author: Kamoru Ridwanullah
+ */
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - Prints numbers, followed by a new line.
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...) 
+{
+int i;
+int value;
+va_list alx;
+va_start(alx, n);
+for (i = 0; i < n; i++)
+{
+value = va_arg(alx,int);
+if (separator == NULL)
+printf("%d ",value);
+else 
+{
+if (i == (n-1))
+printf("%d ",value);
+else
+printf("%d%s ", value, separator);
+}
+}
+va_end(alx);
+
+printf("\n");
+}
