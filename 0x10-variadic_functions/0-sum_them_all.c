aa13628cd304135b97ccde1820040c02721e4642
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-30 17:18:41 +0100

    variadic functions implemented

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,11 +8,11 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-if (n == 0)
-return (0);
 int sum = 0;
 unsigned int i = 0;
 va_list ap;
+if (n == 0)
+return (0);
 va_start(ap, n);
 for (i = 0; i < n; i++)
 sum += va_arg(ap, int);
