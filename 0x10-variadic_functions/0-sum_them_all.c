157f79bc5df3e815d3e36ec627a83fc067d6db29
Kamoru Ridwanullah <kamoruridwanullah1@gmail.com>
2024-01-30 17:12:48 +0100

    variadic functions implemented

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include "variadic_functions.h"
 /**
  * sum_them_all - Calculate the sum of a variable number of integers.
  * @n: The number of integers to sum.
@@ -8,9 +9,9 @@
 int sum_them_all(const unsigned int n, ...)
 {
 if (n == 0)
-return (0)
-int sum = 0;  /* Initialize sum to zero */
-int i = 0;
+return (0);
+int sum = 0;
+unsigned int i = 0;
 va_list ap;
 va_start(ap, n);
 for (i = 0; i < n; i++)
